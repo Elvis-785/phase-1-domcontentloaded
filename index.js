9221368c7bebe194c28301f77c891714d5899de1
Elvis-785 <kiruielvis3358@gmail.com>
2024-11-04 12:59:01 +0300

    DOMContentLoaded event

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(event){
+    console.log("The DOM was loaded");
+    let txtElement = document.querySelector("#text");
+    txtElement.textContent = "This is really cool!"
+})
